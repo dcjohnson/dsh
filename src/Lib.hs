3b6f8ef4c3384583ed3292cfdb8fbadc0585a842
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2023-06-18 14:51:52 -0600

    fork

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,14 @@
 module Lib
-    ( ptyTest
+    ( ptyTest,
+      fork
     ) where
 
 import System.IO
 import Data.String
+import System.Posix.IO as SPI
 import System.Posix.Terminal as SPT
 import System.Posix.IO.ByteString as SPIB
+import System.Posix.Process as SPP
 import Control.Concurrent
  
 ptyTest :: IO ()
@@ -16,3 +19,13 @@ ptyTest = do
                                 return ())
   (str, _) <- SPIB.fdRead m 10
   System.IO.putStrLn str
+
+fork :: IO ()
+fork = do
+  fPid <- SPP.forkProcess (do
+      txtFD <- SPI.openFd "/Users/dcjohnson/dev/wackadoo/text.txt" WriteOnly Nothing (SPI.defaultFileFlags { append = True }) 
+      dupTo txtFD stdOutput 
+      SPP.executeFile "echo" True ["reeeeeee"] Nothing
+      return ())                              
+  SPP.getProcessStatus True False fPid
+  return ()
