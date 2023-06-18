41671e81137cb59095f371f55f6572963aa96ba8
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2023-06-18 13:18:04 -0600

    pty hello world

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    ( ptyTest
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO
+import Data.String
+import System.Posix.Terminal as SPT
+import System.Posix.IO.ByteString as SPIB
+import Control.Concurrent
+ 
+ptyTest :: IO ()
+ptyTest = do
+  (m, s) <- SPT.openPseudoTerminal
+  Control.Concurrent.forkIO (do
+                                SPIB.fdWrite s (fromString "Hello!")
+                                return ())
+  (str, _) <- SPIB.fdRead m 10
+  System.IO.putStrLn str
